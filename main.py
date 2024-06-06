2d03d5f87b98ab7a1142836a41de1819293cf81c
Chris McComb <ccmcc2012@gmail.com>
2024-06-06 16:54:21 -0400

    Try again

--- main.py
+++ main.py
@@ -6,36 +6,44 @@ import datetime  # to get hte current year
 
 import datasets  # to make/upload a dataset
 import pandas  # to handle the dataset
-import lxml.etree
+import lxml.etree  # For error handling
+import tqdm  # for displaying progressm
 
 # Get API token from command line
 HF_TOKEN = sys.argv[1]
-
-# Make an empty list to add dicts too
-dicts = []
 
 # What years do we care about?
 years = ["Historical"] + [
     str(y) for y in list(range(1959, datetime.datetime.now().year + 1))
 ]
 
-for year in years:
-    print(year)
-    filename, headers = urllib.request.urlretrieve(
+# Step through each year, download the associated file, and parse it as set of xl files
+for year in tqdm.tqdm(years, "Downloading annual reports"):
+
+    # Download the file
+    filename, _ = urllib.request.urlretrieve(
         "https://www.nsf.gov/awardsearch/download?DownloadFileName="
         + year
         + "&All=true"
     )
+
+    # Unzip the file that we just downloaded
     with zipfile.ZipFile(filename, "r") as zip_ref:
         zip_ref.extractall(".")
-        for file in os.listdir("./"):
-            if file.endswith(".xml"):
-                try:
-                    dicts.append(pandas.read_xml(file).to_dict(orient="records")[0])
-                    os.remove(file)
-                except lxml.etree.XMLSyntaxError:
-                    print(file)
 
+# Make an empty list to add dicts too
+dicts = []
+
+# For every xml file that we just exposed, turn it into a dict
+for file in tqdm.tqdm(os.listdir("./"), "Loading awards"):
+    if file.endswith(".xml"):
+        try:
+            dicts.append(pandas.read_xml(file).to_dict(orient="records")[0])
+            os.remove(file)
+        except lxml.etree.XMLSyntaxError:
+            print(file)
+
+# Take the dicts, make a dataframe, make a dataset, and upload it
 datasets.Dataset.from_pandas(pandas.DataFrame().from_dict(dicts)).push_to_hub(
     "ccm/nsf-awards", token=HF_TOKEN
 )
