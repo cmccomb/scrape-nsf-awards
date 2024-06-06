2bdfa6bc7eb36e5d06159baa0a6fc4c7f581ff98
Chris McComb <ccmcc2012@gmail.com>
2024-06-06 16:38:03 -0400

    More documentation

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-import os
-import sys
-import urllib.request
-import zipfile
-import datetime
+import os  # for navigating downloaded files
+import sys  # to get input arg
+import urllib.request  # to download files
+import zipfile  # to unzip downloaded files
+import datetime  # to get hte current year
 
-import datasets
-import pandas
+import datasets  # to make/upload a dataset
+import pandas  # to handle the dataset
 
 # Get API token from command line
 HF_TOKEN = sys.argv[1]
 
+# Make an empty list to add dicts too
 dicts = []
 
+# What years do we care about?
 years = ["Historical"] + [
     str(y) for y in list(range(1959, datetime.datetime.now().year + 1))
 ]
