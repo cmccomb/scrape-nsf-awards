019df952d3ed701780770862319c6b1b715f1c5b
Chris McComb <ccmcc2012@gmail.com>
2024-06-08 12:08:40 -0400

    Cleanup

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ import datasets  # to make/upload a dataset
 import tqdm.auto  # for displaying progress
 import xmltodict  # for converting xml
 
-# Features that should be floated during processing
-float_features = {"AwardTotalIntnAmount", "AwardAmount"}
-
 # Get API token from command line
-HF_TOKEN: str = sys.argv[1]
+HUGGINGFACE_TOKEN: str = sys.argv[1]
+
+# Features that should be floated during processing
+float_features: set[str] = {"AwardTotalIntnAmount", "AwardAmount"}
 
 # What years do we care about? Let's get all of them! We also need to download the
 # historical file though (see here: https://www.nsf.gov/awardsearch/download.jsp)
-years: list[str] = ["Historical"] + [
+years_of_interest: list[str] = ["Historical"] + [
     str(y) for y in list(range(1959, datetime.datetime.now().year + 1))
 ]
 
@@ -24,17 +24,17 @@ years: list[str] = ["Historical"] + [
 awards: list[dict] = []
 
 # Step through each year, download the associated file, and parse it as set of xml files
-for year in tqdm.auto.tqdm(years, "Downloading and parsing by year... "):
+for year in tqdm.auto.tqdm(years_of_interest, "Downloading and parsing by year... "):
 
     # Set up a request for the file
-    req = urllib.request.Request(
+    request = urllib.request.Request(
         "https://www.nsf.gov/awardsearch/download?DownloadFileName="
         + year
         + "&All=true"
     )
 
     # Download the file
-    with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen(request) as response:
 
         # Unzip the file that we just downloaded
         with zipfile.ZipFile(io.BytesIO(response.read())) as zip_file:
@@ -42,21 +42,21 @@ for year in tqdm.auto.tqdm(years, "Downloading and parsing by year... "):
             # For every file in the zip, turn it into a dict
             for contained_file in zip_file.infolist():
                 try:
-                    with zip_file.open(contained_file.filename, "r") as f:
-                        award = xmltodict.parse(f.read())["rootTag"]["Award"]
-                        for k in award.keys():
-                            if k in float_features:
-                                award[k] = float(award[k] or 'nan')
+                    with zip_file.open(contained_file.filename, "r") as file:
+                        award: dict = xmltodict.parse(file.read())["rootTag"]["Award"]
+                        for key in award.keys():
+                            if key in float_features:
+                                award[key] = float(award[key] or "nan")
                             else:
-                                award[k] = str(award[k])
-                        print(award)
-                except xmltodict.expat.ExpatError as e:
+                                award[key] = str(award[key])
+
+                # Sometimes the file is not well-formed, so an error is thrown.
+                # We just chalk this up as a loss.
+                except xmltodict.expat.ExpatError:
                     pass
 
 # Create a dataset from the list of dicts
-ds = datasets.Dataset.from_list(awards)
+dataset = datasets.Dataset.from_list(awards)
 
 # Upload the dataset to huggingface
-ds.push_to_hub(
-    "ccm/nsf-awards", token=HF_TOKEN
-)
+dataset.push_to_hub("ccm/nsf-awards", token=HUGGINGFACE_TOKEN)
